de35dc3166df885c354c5e27c3ac3ddc0adaa617
imPerella <99669511+imPerella@users.noreply.github.com>
2024-12-11 19:17:08 +0100

    Update base.py
    1 hour and 15 minutes - Stuck on final state function returning a 4x4 not a 4x1

--- base.py
+++ base.py
@@ -9,18 +9,36 @@ from scipy.linalg import expm
 # Please feel free to ask questions
 # I'd also request that you keep approximate track of how much time this takes.
 
-def U(theta: float, phi: float) -> np.ndarray:
-    """
-    Implement the unitary U(θ, φ) as a 2x2 NumPy array (complex).
-    """
-    pass
+C = np.array([[1], [0]]) # |C>
+D = np.array([[0], [1]]) # |D>
 
-def final_state(U_A: np.ndarray, U_B: np.ndarray, J: np.ndarray, initial_state: np.ndarray) -> np.ndarray:
+# U(θ, φ)
+def U(theta: float, phi: float) -> np.ndarray:
+    uni = np.array([[np.exp(1j * phi) * np.cos(theta/2), np.sin(theta/2)],
+                    [-np.sin(theta/2), np.exp(-1j * phi) * np.cos(theta/2)]])
+    return uni
+#done in roughly 19 minutes; most of the time was just spent looking up syntax
+
+#Define Operators/strategies
+C_op = U(0, 0)
+D_op = U(np.pi, 0)
+Q_op = U(0, np.pi/2)
+
+#gamma within the range 0 -> pi/2; entanglement increases as gamma increases
+#Get unitary Operator J; 9 minutes
+def J(gamma):
+    return expm(np.kron(-1j*gamma*D_op, D_op/2))
+
+# get final state vector; should be 4x1 array (complex)
+def final_state(U_A: np.ndarray, U_B: np.ndarray, J: np.ndarray, initial_state: np.ndarray):
     """
     Compute the final state vector.
     This should return a 4x1 NumPy array (complex).
     """
-    pass
+    fs_vect = J.transpose() * np.kron(U_A, U_B) * J * inital_state
+    return fs_vect
+    #46 minutes; yet to output a 4x1 matrix, however does output a 4x4
+print(final_state(C_op, D_op, J(0), np.kron(C, C))) #test that doesn't produce a 4x1
 
 def payoff_A(final_state: np.ndarray) -> float:
     """
